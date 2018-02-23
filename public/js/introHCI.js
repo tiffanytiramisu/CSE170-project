5bed391c512b91a2b61c0de6b7b3c55da53485a5
Shiwei Zhou <s1zhou@ucsd.edu>
2018-02-22 19:36:14 -0800

    fb login added

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,43 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// add any functionality and listeners you want here
+	$('.project a').click(addProjectDetails);
+
+	$('#colorBtn').click(randomizeColors);
 }
+
+/*
+ * Make an AJAX call to retrieve project details and add it in
+ */
+function addProjectDetails(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	// Get the div ID, e.g., "project3"
+	var projectID = $(this).closest('.project').attr('id');
+	// get rid of 'project' from the front of the id 'project3'
+	var idNumber = projectID.substr('project'.length);
+
+	console.log("User clicked on project " + idNumber);
+	$.get("https://cse170lab1shiweizhou.herokuapp.com/project/"+idNumber, callBackFn);
+}
+
+function callBackFn(result){
+	console.log(result.image);
+	console.log("#"+result.id+" "+".details");
+	$("#project"+result.id+" "+".details").append("<img class=detailsImage src="+result.image+" />");
+	$("#project"+result.id+" "+".details").append("<h5>"+result.title+"</h5>");
+	$("#project"+result.id+" "+".details").append(result.summary);
+}
+
+// function addProject(result) {
+//   var projectHTML = '<a href="#" class="thumbnail">' +
+//     '<img src="' + result['image'] + '" class="img">' +
+//     '<p>' + result['title'] + '</p>' +
+//     '<p><small>' + result['date'] +
+//     '</small></p></a>'; 
+// }
+
+// $.get("http://URL", callBackFn)
+
+// $.post("http://URL", {"json":"json"}, callBackFn)
